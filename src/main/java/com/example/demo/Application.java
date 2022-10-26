9ce3d4acafb4d70a97f56a51edb271a6add46649
DexterWuu <wujiaxuanfighting@gmail.com>
2022-10-27 10:55:20 +1100

    added Page request and sort fuction in application

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,7 +1,15 @@
 package com.example.demo;
 
+import com.github.javafaker.Faker;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +17,36 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            generateRandomStudents(studentRepository);
+            //sorting(studentRepository);
+            PageRequest pageRequest = PageRequest.of(0, 5, Sort.by("firstName").ascending());
+            Page<Student> page = studentRepository.findAll(pageRequest);
+            System.out.println(page);
+        };
+    }
 
+    private void sorting(StudentRepository studentRepository) {
+        Sort sort = Sort.by("firstName").ascending().and(Sort.by("dateOfBirth").descending());
+        studentRepository.findAll(sort).forEach(student -> System.out.println(student.getFirstName() + " " + student.getDateOfBirth()));
+    }
+
+    private void generateRandomStudents(StudentRepository studentRepository) {
+        Faker faker =  new Faker();
+        for (int i = 0; i < 20; i++) {
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String email = String.format("%s.%s@newchannel.edu", firstName, lastName);
+            Date date =  faker.date().birthday();
+           Student student =  new Student (
+                    firstName,
+                    lastName,
+                    date,
+                    email
+            );
+            studentRepository.save(student);
+        }
+    }
 }
---
+++ src/main/java/com/example/demo/StudentRepository.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
+    @Query("SELECT s FROM Student s WHERE s.firstName = ?1 AND s.email = ?2")
+    Optional<Student> findStudentsByFirstNameEqualsAndEmailEquals(String firstName, String email);
+    //native query
+    @Query(value = "SELECT * FROM stundet where first_name = ?1 AND email = ?2", nativeQuery = true)
+    Optional<Student> findStudentsByFirstNameEqualsAndEmailEqualWithNativeQuery(String firstName, String email);
+    @Query("SELECT s FROM Student s WHERE s.firstName = :firstName AND s.email = :email")
+    Optional<Student> findByFirstNameAndEmail(@Param("firstName") String firstName, @Param("email") String email);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Student n WHERE n.studentId = :id")
+    void deleteStudentById(@Param("id") Long id);
+
+}
