ce9f878d13fc3f9b399079a53a7905c233db1cac
DexterWuu <wujiaxuanfighting@gmail.com>
2022-10-28 15:11:55 +1100

    Add class StudentAccount and @OneToOne relation with class Student.

---
+++ src/main/java/com/example/demo/StudentAccount.java
@@ -0,0 +1,62 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity(name = "StudentAccount")
+@Table(name  = "student_account")
+public class StudentAccount {
+    @Id
+    private Long id;
+    @Column(name = "account_name", nullable = false, columnDefinition = "TEXT")
+    private String accountName;
+
+    @Column (name = "password", nullable = false, columnDefinition = "TEXT")
+    private String password;
+    @OneToOne(cascade = CascadeType.ALL)
+    @MapsId
+    Student student;
+
+    public StudentAccount(String password, Student student) {
+        this.password = password;
+        this.student = student;
+        this.accountName = student.getEmail();
+
+    }
+
+    public StudentAccount() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    @JsonIgnore
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+}
---
+++ src/main/java/com/example/demo/StudentIdCard.java
@@ -0,0 +1,55 @@
+package com.example.demo;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity(name = "StudentIdCard")
+@Table(name = "student_id_card",
+        uniqueConstraints = {@UniqueConstraint(name = "unique_student_card_number", columnNames = "card_number")})
+
+public class StudentIdCard {
+    @Id
+    @SequenceGenerator (name = "student_card_id_sequence", sequenceName = "student_card_id_sequence",allocationSize = 1)
+    @GeneratedValue (strategy = SEQUENCE, generator = "student_id_card_id_sequence")
+    @Column(name = "id", updatable = false)
+    Long cardId;
+
+    @Column(name = "card_number", updatable = false, length = 15)
+    private String cardNumber;
+
+    @OneToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "student_id",
+                referencedColumnName = "student_id")
+    private Student student;
+
+    public Long getCardId() {
+        return cardId;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardId(Long cardId) {
+        this.cardId = cardId;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public StudentIdCard(Long cardId, String cardNumber) {
+        this.cardId = cardId;
+        this.cardNumber = cardNumber;
+    }
+
+
+    public StudentIdCard(String cardNumber, Student student) {
+        this.cardNumber = cardNumber;
+        this.student = student;
+    }
+
+    public StudentIdCard() {
+    }
+}
